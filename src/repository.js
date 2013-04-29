b8d90a82c77c05b2d5ef97f09487721bf0951c1b
Ismael Celis <ismaelct@gmail.com>
2013-04-29 18:26:18 +0100

    Bare parent Repository

--- src/repository.js
+++ src/repository.js
@@ -1,21 +1,28 @@
+/*
+var index = new Bootic.Index()
+
+var Ajax = Bootic.Repository.extend({
+  _add: function (item, promise) {
+    $.post('/foo', item.attributes).then(promise)
+  }
+})
+
+var repo = new Ajax(index)
+
+users = new Bootic.Pipe()
+
+index.pipe(users).pipe(view)
+
+*/
 Bootic.Repository = (function ($) {
   "use strict";
   
-  function ajax(item, fn) {
-    setTimeout(fn, 5000)
-  }
-  
   var Repository = Bootic.Pipe.extend({
     
-    initialize: function (index) {
+    initialize: function (index, options) {
       this.index = index
+      this.options = options || {}
       this.pipe(this.index)
-    },
-    
-    _add: function (item, promise) {
-      ajax(item, function () {
-        promise.resolve()
-      })
     }
     
   })
