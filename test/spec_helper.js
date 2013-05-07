2bcce2e8c0a26ccfc3f7be00116bd03b42321206
Ismael Celis <ismaelct@gmail.com>
2013-05-07 17:12:00 +0100

    Better tests

--- test/spec_helper.js
+++ test/spec_helper.js
@@ -6,14 +6,20 @@ beforeEach(function () {
 function behavesLikeAPipe(context) {
 
   describe('behaves like a Pipe', function () {
-    var pipe1, pipe2, pipe3, addSpy, removeSpy, struct;
+    var pipe1, pipe2, pipe3, spy, addSpy, removeSpy, struct;
 
     beforeEach(function () {
+      spy       = {add: $.noop, remove: $.noop}
       pipe1     = context.pipe1;
       pipe2     = new Bootic.Pipe()
       pipe3     = new Bootic.Pipe()
-      addSpy    = jasmine.createSpy('add')
-      removeSpy = jasmine.createSpy('remove')
+      
+      addSpy    = jasmine.createSpy('add spy')
+      removeSpy = jasmine.createSpy('remove spy')
+      
+      spyOn(spy, 'add')
+      spyOn(spy, 'remove')
+      
       struct    = new Bootic.Struct({id: 1})
     })
     
@@ -50,26 +56,24 @@ function behavesLikeAPipe(context) {
     describe('#pipe()', function () {
 
       it('forwards added structs from one pipe to the other', function () {
-
-        pipe3.on('add', addSpy)
-
+        spyOn(pipe3, 'add')
         pipe1.pipe(pipe2).pipe(pipe3);
 
         pipe1.add(struct);
 
-        expect(addSpy).toHaveBeenCalledWith(struct);
+        expect(pipe3.add).toHaveBeenCalledWith(struct);
       })
       
       it('forwards removed structs from one pipe to the other', function () {
 
-        pipe3.on('remove', removeSpy)
+        spyOn(pipe3, 'remove')
 
         pipe1.pipe(pipe2).pipe(pipe3);
 
         pipe1.add(struct);
         pipe1.remove(struct);
 
-        expect(removeSpy).toHaveBeenCalledWith(struct);
+        expect(pipe3.remove).toHaveBeenCalledWith(struct);
       })
       
     })
@@ -88,14 +92,14 @@ function behavesLikeAPipe(context) {
       })
       
       it('does not pipe newly added structs to other Pipes', function () {
-        var spy = jasmine.createSpy('add spy'),
-            newStruct = new Bootic.Struct()
-            
-        pipe2.on('add', spy)
+        var newStruct = new Bootic.Struct()
+        
+        spyOn(pipe2, 'add')
+        
         pipe1.pipe(pipe2)
         
         pipe1.add(newStruct)
-        expect(spy).not.toHaveBeenCalledWith(newStruct)
+        expect(pipe2.add).not.toHaveBeenCalledWith(newStruct)
       })
       
       it('triggers `reject` utility event', function () {
