6d4d8335aa4fa876adc2497bb9ed1f6685b273aa
Ismael Celis <ismaelct@gmail.com>
2013-05-08 22:45:58 +0100

    Documented Router.

--- src/devices/router.js
+++ src/devices/router.js
@@ -1,3 +1,33 @@
+/*!
+ * Plumber.Devices.Router
+ * Copyright (C) 2013 Ismael Celis
+ *
+ * Declare routing functions and pipe instances to route to.
+ *
+ * Usage:
+ *
+ *     var p1 = new Plumber.Pipe()
+ *     var p2 = new lumber.Pipe()
+ *     var default = new Plumber.Pipe()
+ *     var router = new Plumber.Devices.Router()
+ *
+ *     router
+ *      .route(p1, function (struct, promise) {
+ *          if(struct.get('name') == 'Joe') promise.resolve()
+ *          else promise.reject()
+ *       })
+ *       .route(p2, function (struct, promise) {
+ *          if(struct.get('name') == 'Jane') promise.resolve()
+ *          else promise.reject()
+ *       })
+ *       .default(default)
+ *
+ *     router.add(new Plumber.Struct({name: 'Joe'})) // forwards struct to p1
+ *     router.add(new Plumber.Struct({name: 'Jane'})) // forwards struct to p2
+ *     router.add(new Plumber.Struct({name: 'Paul'})) // forwards struct to default
+ *
+ * @constructor
+ */
 Plumber.Devices.Router = (function ($) {
   "use strict";
   
