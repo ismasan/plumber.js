bcad4541a5a197cea69f5274d80cc5d912caf5df
Ismael Celis <ismaelct@gmail.com>
2013-05-08 22:39:48 +0100

    Documented Pipeline device.

--- src/devices/pipeline.js
+++ src/devices/pipeline.js
@@ -1,5 +1,39 @@
+/*!
+ * Plumber.Devices.Pipeline
+ * Copyright (C) 2013 Ismael Celis
+ *
+ * Wraps multiple pipes, chain their #add and #remove calls sequentially while allowing for async execution.
+ *
+ * Usage:
+ *
+ *     var p1 = new Plumber.Pipe()
+ *     var p2 = new SomeAjaxPipe()
+ *     var results = new Plumber.Pipe()
+ *     var pipeline = new Plumber.Devices.Pipeline(p1, p2)
+ *
+ *     pipeline.add(struct) // will forward struct to p1, p2 and finally pipe on to results pipe
+ *
+ * @constructor
+ */
 Plumber.Devices.Pipeline = (function ($) {
   "use strict";
+  
+  function chain (verb, arr, struct, finalPromise) {
+    var fn,
+        first = arr.shift();
+  
+    if(arr.length == 0) { // last one.
+      fn = function (struct) {
+        finalPromise.resolve(struct, verb)
+      }
+    } else { // keep chaining recursively
+      fn = function (struct) {
+        chain(verb, arr, struct, finalPromise)
+      }
+    }
+  
+    first[verb](struct).then(fn) // add, remove
+  }
   
   var Pipeline = Plumber.Pipe.extend({
     initialize: function () {
@@ -7,30 +41,13 @@ Plumber.Devices.Pipeline = (function ($) {
     },
     
     _add: function (struct, promise) {
-      this.__chain('add', this.__wrapped.slice(0), struct, promise)
+      chain('add', this.__wrapped.slice(0), struct, promise)
     },
     
     _remove: function (struct, promise) {
-      this.__chain('remove', this.__wrapped.slice(0), struct, promise)
-    },
-    
-    __chain: function (verb, arr, struct, finalPromise) {
-      var fn,
-          self = this,
-          first = arr.shift();
-    
-      if(arr.length == 0) { // last one.
-        fn = function (struct) {
-          finalPromise.resolve(struct, verb)
-        }
-      } else { // keep chaining recursively
-        fn = function (struct) {
-          self.__chain(verb, arr, struct, finalPromise)
-        }
-      }
-    
-      first[verb](struct).then(fn) // add, remove
+      chain('remove', this.__wrapped.slice(0), struct, promise)
     }
+    
   })
   
   return Pipeline
