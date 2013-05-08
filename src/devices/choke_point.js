882ab9e2f77b3b8d0510a21a023e705ef8d6b703
Ismael Celis <ismaelct@gmail.com>
2013-05-08 22:35:54 +0100

    Documented ChokePoint device.

--- src/devices/choke_point.js
+++ src/devices/choke_point.js
@@ -1,5 +1,28 @@
+/*!
+ * Plumber.Devices.ChokePoint
+ * Copyright (C) 2013 Ismael Celis
+ *
+ * Wraps multiple pipes, runs their #add() methods in parallel and waits for all of them before finalizing the #add lifecycle.
+ *
+ * Usage:
+ *
+ *     var p1 = new Plumber.Pipe()
+ *     var p2 = new SomeAjaxPipe()
+ *     var results = new Plumber.Pipe()
+ *     var choke = new Plumber.Devices.ChokePoint(p1, p2)
+ *
+ *     choke.add(struct) // won't pipe to `results` until both p1 and p2 have finished adding., which may be asynchronous.
+ *
+ * @constructor
+ */
 Plumber.Devices.ChokePoint = (function ($) {
   "use strict";
+  
+  function promisesFor(pipes, verb, struct) {
+    return $.map(pipes, function (pipe) {
+      return pipe[verb](struct)
+    })
+  }
   
   var ChokePoint = Plumber.Pipe.extend({
     initialize: function () {
@@ -7,20 +30,14 @@ Plumber.Devices.ChokePoint = (function ($) {
     },
     
     _add: function (struct, promise) {
-      $.when.apply($, this.__promisesFor('add', struct)).then(function () {
+      $.when.apply($, promisesFor(this.__wrapped, 'add', struct)).then(function () {
         promise.resolve(struct)
       })
     },
     
     _remove: function (struct, promise) {
-      $.when.apply($, this.__promisesFor('remove', struct)).then(function () {
+      $.when.apply($, promisesFor(this.__wrapped, 'remove', struct)).then(function () {
         promise.resolve(struct)
-      })
-    },
-    
-    __promisesFor: function (verb, struct) {
-      return $.map(this.__wrapped, function (pipe) {
-        return pipe[verb](struct)
       })
     }
   })
